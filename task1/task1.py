f3f390f17fd54c9eb4862c4128c9c2733ab78789
MikhailSh <86521600+stregelemene21@users.noreply.github.com>
2022-02-15 18:58:50 +0300

    Create task1.py

---
+++ task1/task1.py
@@ -0,0 +1,11 @@
+n, m = map(int,input().split())
+s = [i for i in range(1, n+1)]
+listResult = [1]
+while True:
+    for i in range(0, m-1):
+        s.append(s.pop(0))
+    if s[0] == 1 :
+        break
+    else :
+        listResult.append(s[0])
+print(*listResult)
