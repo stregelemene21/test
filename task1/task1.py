4183303560e1095699e90e08f6d8e372a35d623d
MikhailSh <86521600+stregelemene21@users.noreply.github.com>
2022-02-17 23:41:43 +0300

    task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,15 @@
-n, m = map(int,input().split())
-s = [i for i in range(1, n+1)]
+import sys
+
+n = sys.argv[1]
+m= sys.argv[2]
+
+s = [i for i in range(1, int(n)+1)]
 listResult = [1]
 while True:
-    for i in range(0, m-1):
+    for i in range(0, int(m)-1):
         s.append(s.pop(0))
     if s[0] == 1 :
         break
     else :
         listResult.append(s[0])
-print(*listResult) 
+print(*listResult)
